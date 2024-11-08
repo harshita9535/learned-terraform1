1987f8e385453bd19668983400b0d447aed361a2
Harshita <harshitabhadimane02051998@gmail.com>
2024-11-08 08:32:39 +0530

    Provisioner demo

--- provisioner/main.tf
+++ provisioner/main.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "test" {
-  ami = "09c813fb71547fc4f"
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.selected.id]
 
 
   provisioner "remote-exec" {
+
     connection {
       type     = "ssh"
       user     = "ec2-user"
@@ -18,8 +19,6 @@ resource "aws_instance" "test" {
     ]
   }
 }
-
-
 
 data "aws_security_group" "selected" {
   name = "allow-all"
